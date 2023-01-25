4a4635d16088b99eb1a6e5290cd7286f23f4a8ac
Hriday Hegde <hridayhegde1999@gmail.com>
2023-01-25 21:04:37 +0530

    Added another basic project

--- Notes/ReactJS.md
+++ Notes/ReactJS.md
@@ -33,3 +33,19 @@
 - JSX ternary expressions are allowed
 
 - && can be used in a condition statement to output the item after && when the first condition is true
+
+
+- JSX Limitations
+  - multiple components cannot exist side by side when returning jsx
+  - This can be worked around by adding a div around those components, however a better approach is to add a custom wrapper component, This component only return props.children back without adding any new elements to the content
+  - Div Soup(solution : Fragments)
+    - This can be mitigated by the same solution as above, however react has a similar component available called <React.Fragment> which can be added and serves the same purpose
+  - Portals
+  - Refs - use where you done want to set state every keystroke
+
+- Use State
+  - Storing state variables so that they update the object when changed
+- Use Reducer
+  - This is used as an alternative to useState when you need to have complex data stored in a state, it can also have a function to solve the state based sequences and what to return
+- useEffect
+  - This is used to trigger events based off of other events. Here the Effect is a side effect such that suppose a variable(state) is changed then this effect will run
---
+++ Practice/react-basic-hooks/src/components/Login/Login.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect, useReducer } from "react";
+
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+import Input from "../UI/Input/Input";
+
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+
+  return { value: "", isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+
+  return { value: "", isValid: false };
+};
+
+const Login = (props) => {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
+
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  //console.log(emailState);
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setFormIsValid(emailIsValid && passwordIsValid);
+    }, 500);
+
+    return () => {
+      clearTimeout(identifier);
+    };
+  }, [emailIsValid, passwordIsValid]);
+
+  const emailChangeHandler = (event) => {
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  const passwordChangeHandler = (event) => {
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  const validateEmailHandler = () => {
+    dispatchEmail({ type: "INPUT_BLUR" });
+  };
+
+  const validatePasswordHandler = () => {
+    dispatchPassword({ type: "INPUT_BLUR" });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(emailState.value, passwordState.value);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <Input id='email' type='email' isValid= {emailIsValid} onChange={emailChangeHandler} onBlur={validateEmailHandler} label="E-Mail" />
+        <Input id='password' type='password' isValid= {passwordIsValid} onChange={passwordChangeHandler} onBlur={validatePasswordHandler} label="Password" />
+        
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
